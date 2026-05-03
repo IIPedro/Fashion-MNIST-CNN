70ecfae5087963cd5bd5ae8497bd37580665c068
IIPedro <65401923+IIPedro@users.noreply.github.com>
2026-05-03 11:42:37 -0300

    add more channels to neural network's hidden layers

--- neural_network.py
+++ neural_network.py
@@ -9,28 +9,23 @@ class MNIST_CNN(nn.Module):
             # 28 x 28 x 16
             nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU(),
-            # 28 x 28 x 32
-            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1, bias=True),
-            nn.ReLU(),
-            # 14 x 14 x 32
+            # 14 x 14 x 16
             nn.MaxPool2d(kernel_size=2),
             # 14 x 14 x 32
-            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU(),
-            # 14 x 14 x 32
-            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1, bias=True),
+            # 7 x 7 x 32
+            nn.MaxPool2d(kernel_size=2),
+            # 7 x 7 x 64
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU(),
-            # 14 x 14 x 32 = 6272
+            # 7 x 7 x 64 = 3136
             nn.Flatten(),
-            nn.Linear(6272, 6272),
-            nn.ReLU(),
-            nn.Dropout(p=0.5),
-            # 6272
-            nn.Linear(6272, 6272),
+            nn.Linear(3136, 3136),
             nn.ReLU(),
             nn.Dropout(p=0.5),
             # 10
-            nn.Linear(6272, 10),
+            nn.Linear(3136, 10),
         )
 
     def forward(self, x):
